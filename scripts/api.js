5d3ff1972e5819c50da33f1e532fd68c1fbbd8ba
Theders98 <djribellesclase@gmail.com>
2020-02-05 21:33:32 +0100

    asddasdasdas

--- scripts/api.js
+++ scripts/api.js
@@ -1,38 +1,20 @@
 
 function postData(data){
-    $.ajax({
+    return $.ajax({
         type: "POST",
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
+        url: "http://localhost:3000/peliculas",
+        data: data
       });
 }
 
-function getData(){
-  $.ajax({
-    type: "GET",
-    url: "http://localhost:3000/peliculas",
-    success: function (data) {
+//Con este metodo de hacer la peticion retornamos la promesa y la recorremos desde el otro js usando el .done(), en teoria este es el metodo mas nuevo y el mas usado
+//Y sustituyó al otro metodo de hacer las peticiones
 
-        for (let i = 0; i < data.length; i++) {
-
-            $("#content").append( 
-                
-                `<div id="${data[i].id}">
-                     <tr> 
-                        <td>Nombre: ${data[i].nombre}</td>
-                        <td>Director:  ${data[i].director}</td>
-                        <td>Clasificacion:  ${data[i].clasificacion}</td>
-                        <td><button id='botonBorrar' class = 'borrar'>Borrar pelicula</button></td>
-                        <td><button id='botonModificar' class = 'modificar'>Modificar pelicula</button></td>
-                     </tr>
-                </div>
-            `
-            )
-        }
-    }
-})
+function getDataAlternativa(){
+    return  $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/peliculas",
+    })
 }
 
 function deleteData(data){
@@ -45,13 +27,42 @@ function deleteData(data){
 })
 }
 
-function putData(data){
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: data,
-        success: success,
-        dataType: dataType
+function putData(id, data){
+    console.log(JSON.stringify(data))
+    return $.ajax({
+        type: "PUT",
+        url: `http://localhost:3000/peliculas/${id}`,
+        data: data
       });
 }
 
+
+
+
+
+// Este emtodo es con succes y esta en desuso
+function getData(){
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:3000/peliculas",
+      success: function (data) {
+  
+          for (let i = 0; i < data.length; i++) {
+  
+              $("#content").append( 
+                  
+                  `<div id="${data[i].id}">
+                       <tr> 
+                          <td>Nombre: ${data[i].nombre}</td>
+                          <td>Director:  ${data[i].director}</td>
+                          <td>Clasificacion:  ${data[i].clasificacion}</td>
+                          <td><button id='botonBorrar' class = 'borrar'>Borrar pelicula</button></td>
+                          <td><button id='botonModificar' class = 'modificar'>Modificar pelicula</button></td>
+                       </tr>
+                  </div>
+              `
+              )
+          }
+      }
+  })
+  }
--- scripts/componentes.js
+++ scripts/componentes.js
@@ -1,20 +1,94 @@
-$(document).ready( function(){
-    
-getData()
-deleteButton()
-modifyButton()
+$(document).ready(function () {
+    let lastId = 0;
+    $('#formData').hide();
+    showData()
 
-});
 
-function deleteButton(){
 
-    $(document).on('click','#botonBorrar',function(){
-        let idObjeto = $(this).parent().attr('id');
-        deleteData(idObjeto)
-    })
-}
-function modifyButton(){
-    $('.modal').on('shown.bs.modal', function () {
-        $('#botonModificar').trigger('focus')
-      })
-}
+    function showData() {
+
+        //recorre la promesa y trae los datos del json
+        getDataAlternativa().done(function (data) {
+
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].id >= lastId) { lastId = data[i].id }
+                $("#content").append(
+
+                    `
+                <tr id="${data[i].id}"> 
+
+                        <td>${data[i].nombre}</td>
+                        <td>${data[i].director}</td>
+                        <td>${data[i].clasificacion}</td>
+                        <td><button id='botonBorrar' class = 'borrar'>Borrar pelicula</button></td>
+                        <td><button id='botonModificar' class = 'modificar'>Modificar pelicula</button></td>                   
+                         
+                </tr>             
+            `
+                )
+            }
+        });
+        //aqui se usan los metodos, que administran las peticiones, realmente los pongo aqui porque quiero, no hay ninguna razón lógica
+        deleteButton()
+        modifyButton()
+        addButton()
+    }
+    function deleteButton() {
+
+        $(document).on('click', '#botonBorrar', function () {
+            //esto selecciona la id del objeto abuelo para obtener la id del tr que es el que contiene todos los datos del get
+            let idObjeto = $(this).parent().parent().attr('id');
+
+            //peticion de delete, solo le pasamos la id
+            deleteData(idObjeto)
+
+        })
+    }
+    function modifyButton() {
+
+        $(document).on('click', '#botonModificar', function () {
+            //esto selecciona la id del objeto abuelo para obtener la id del tr que es el que contiene todos los datos del get
+            let idObjeto = $(this).parent().parent().attr('id');
+            $('.form__id').val(idObjeto)
+            $('#formData').show();
+            $('#addFilmBttn').hide();
+
+            $('.enviar').click(function () {
+
+                putData(idObjeto, $('#formData').serialize()).done(function () {
+                    console.log('xd')
+                    $('#formData').hide();
+                    $('#addFilmBttn').show();
+                })
+
+            })
+
+        })
+    }
+    //Añade funcionalidades al formulario de añadir un elemento al json
+    function addButton() {
+
+        $('#addFilmBttn').click(function () {
+
+            $('#formData').show();
+            $('#addFilmBttn').hide();
+
+            $('.enviar').click(function () {
+                // esta es una manera de hacerlo autoincremental, se basa en obtener el valor mas alto de las id que han pasado por el get y sumarle 1 cuando entre al añadir
+                lastId++
+                //Aqui junto el formulario serializado junto a la id sumada
+                let data = `id=${lastId++}&` + "" + $('#formData').serialize()
+
+                //peticion con done y con funcionalidad para hacer desaparecer el formulario
+                postData(data).done(function () {
+                    alert('añadido con exito')
+                    $('#formData').hide();
+                    $('#addFilmBttn').show();
+                })
+
+            })
+
+
+        })
+    }
+})
