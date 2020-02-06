8731f18d5c88299a966c06e67ac46420d62bc826
Theders98 <djribellesclase@gmail.com>
2020-02-06 19:45:03 +0100

    penis

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,7 @@
 
 //Peticion que actua como todas, el tipo es el tipo de dato que insertas, recurso es la ruta a la que vas a hacer la peticion
 //id es si se solicita id la pides, si no la insertas es vacio, data son los datos a insertar
-
+// para hacer un filtro que no sea por id se insertaria en el tributo id esto -> ?nombre=${nombre}`<- Teniendo en cuenta que nombre seria el filtro deseado
 function peticionGlobal(tipo, recurso, id="", data="") {
     if (tipo == "POST" | tipo == "PUT") { 
         return $.ajax({
@@ -9,7 +9,6 @@ function peticionGlobal(tipo, recurso, id="", data="") {
             url: `http://localhost:3000/${recurso}/${id}`,
             data: data,
             dataType: "JSON"
- 
         })
     } else {
         return $.ajax({
